9f1a5ec6cd706857624e98cd8f8f60c352b6f48e
Miguel23nc <1392023@senati.pe>
2025-02-15 12:07:34 -0500

    asistencia offline 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,49 @@
+const CACHE_NAME = "mi-app-cache-v1";
+const STATIC_FILES = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/ISOTIPO SOFT TOWER.svg",
+  "/GIFT LOGO.gif",
+  "/SOFTOWER-LOGIN.jpg",
+  "/SOFTOWER-LOGIN2.jpg",
+  "/offline.html",
+];
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker instalado");
+  console.log("Service Worker instalado y cacheando archivos");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_FILES);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("Interceptando petición:", event.request.url);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        return caches.match("/index.html"); // Devuelve el HTML base si todo falla
+      })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log("Borrando caché antigua:", cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
